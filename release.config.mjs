b6a7af10d52c5fa658653d560919e60f526cd982
Eric King <eric@webdeveric.com>
2025-12-26 15:44:10 -0600

    chore: added `semantic-release` config

---
+++ release.config.mjs
@@ -0,0 +1,37 @@
+/**
+ * @type {Partial<import('semantic-release').GlobalConfig>}
+ */
+export default {
+  branches: ['master'],
+  preset: 'conventionalcommits',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'chore',
+            scope: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
+          {
+            type: 'chore',
+            scope: 'spelling',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+};
