67245f8711fb785fee5f64ed971c321fad03c7df
Eric King <eric@webdeveric.com>
2025-12-26 12:38:58 -0600

    refactor: allow `json` option to be a tuple

--- src/createConsole.ts
+++ src/createConsole.ts
@@ -5,35 +5,52 @@ import { AnsiJsonWritable, type ModifyOutputFn } from './AnsiJsonWritable.js';
 import type { StyleOptions } from './Style.js';
 import type { JsonReplacerFn } from './types.js';
 
+export type JsonConsoleOptions = {
+  style?: Partial<StyleOptions>;
+  space?: number | string;
+  replacer?: JsonReplacerFn;
+};
+
 export type CreateConsoleOptions = ConsoleConstructorOptions & {
   eol?: string;
   modifyOutput?: ModifyOutputFn;
-  json?: {
-    style?: Partial<StyleOptions>;
-    space?: number | string;
-    replacer?: JsonReplacerFn;
-  };
+  json?: JsonConsoleOptions | [stdout: JsonConsoleOptions, stderr?: JsonConsoleOptions];
 };
 
+/**
+ * Creates a Console instance that supports ANSI styling and JSON formatting.
+ */
 export function createConsole(options: Partial<CreateConsoleOptions> = {}): Console {
   const { stdout = process.stdout, stderr = process.stderr, eol, modifyOutput, json, ...consoleOptions } = options;
+
+  const stdOutJson = Array.isArray(json) ? json[0] : json;
+  const stdErrJson = Array.isArray(json)
+    ? {
+        ...stdOutJson,
+        ...json[1],
+        style: {
+          ...stdOutJson?.style,
+          ...json[1]?.style,
+        },
+      }
+    : json;
 
   return new Console({
     stdout: new AnsiJsonWritable({
       output: stdout,
-      space: json?.space,
-      replacer: json?.replacer,
+      space: stdOutJson?.space,
+      replacer: stdOutJson?.replacer,
       eol,
       modifyOutput,
-      styleOptions: json?.style,
+      styleOptions: stdOutJson?.style,
     }),
     stderr: new AnsiJsonWritable({
       output: stderr,
-      space: json?.space,
-      replacer: json?.replacer,
+      space: stdErrJson?.space,
+      replacer: stdErrJson?.replacer,
       eol,
       modifyOutput,
-      styleOptions: json?.style,
+      styleOptions: stdErrJson?.style,
     }),
     ...consoleOptions,
   });
