10987d133b47e2dbf07505ae13bbcdc2044b6fd8
Eric King <eric@webdeveric.com>
2025-12-26 12:37:04 -0600

    chore: updated types

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,14 @@ import type { styleText } from 'node:util';
 export type JsonReplacerFn = (this: unknown, key: string, value: unknown) => unknown;
 
 export type StyleTextFormat = Parameters<typeof styleText>[0];
+
+/**
+ * @internal
+ */
+type TextFormatStrings = Exclude<StyleTextFormat, string[]>;
+
+export type StyleTextBgColor = Extract<TextFormatStrings, `bg${string}`>;
+
+export type StyleTextColor = StyleTextBgColor extends `bg${infer Color}` ? Uncapitalize<Color> : never;
+
+export type StyleTextModifier = Exclude<TextFormatStrings, StyleTextBgColor | StyleTextColor>;
